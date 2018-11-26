f5d45ec43fab8118a890d9518e55c4f9a2205573
filbs111 <ed.filby@gmail.com>
2018-11-26 02:55:43 +0000

    log page address via background script

--- background.js
+++ background.js
@@ -1,5 +1,33 @@
 console.log("SIDECHANNEL BACKGROUND : in script!");
 
+
+function updateActiveTab(tabs) {
+	console.log("SIDECHANNEL BACKGROUND : in updateActiveTab!");
+	
+	browser.tabs.query({active: true})	//active - Whether the tabs are active in their windows.	//what happens if have 2 windows open? can have multiple active tabs??? would like to have crowserAction button function in multiple open windows 
+    .then(function(tabs){
+		currentUrl = new URL(tabs[0].url);	//how many tabs are listed???
+		console.log("SIDECHANNEL BACKGROUND : " + currentUrl);
+	});
+	//TODO handle what happens if promises return in wrong order.
+	//is it possible to attach some id to the promise????
+}
+
+//taken from bookmark-it example:
+
+// listen to tab URL changes
+browser.tabs.onUpdated.addListener(updateActiveTab);
+
+// listen to tab switching
+browser.tabs.onActivated.addListener(updateActiveTab);
+
+// listen for window switching
+browser.windows.onFocusChanged.addListener(updateActiveTab);
+
+// update when the extension loads initially
+updateActiveTab();
+
+
 //AFAIK if a popup is sent, this event never gets sent!!!!!!!! !!!!!!!!!!
 //how to have a popup and have a background script?
 //want to be able to fire a search on page load so that clicking the browser
